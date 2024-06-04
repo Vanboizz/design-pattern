a3d8d431e8d58401f5345afb0d596db5420af532
TuanKhang123 <20521437@gm.uit.edu.vn>
2024-06-04 08:03:27 +0700

    'Khang_Command'

---
+++ Done/LibraryManagement/LibraryManagement/Command/BorrowCommand.cs
@@ -0,0 +1,27 @@
+﻿using DemoDesign;
+using LibraryManagement.Forms;
+using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibraryManagement.Command
+{
+    public class BorrowCommand : ICommand
+    {
+        private BorrowReturnBook form; 
+
+        public BorrowCommand(BorrowReturnBook form)
+        {
+            this.form = form;
+        }
+
+        public void Execute()
+        {
+            form.CreateBorrowCardMethod();
+        }
+    }
+}
---
+++ Done/LibraryManagement/LibraryManagement/Command/ReturnCommand.cs
@@ -0,0 +1,24 @@
+﻿using DemoDesign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Command
+{
+    public class ReturnCommand : ICommand
+    {
+        private BorrowReturnBook form;
+
+        public ReturnCommand(BorrowReturnBook form)
+        {
+            this.form = form;
+        }
+
+        public void Execute()
+        {
+            form.CreateReturnCardMethod();
+        }
+    }
+}
