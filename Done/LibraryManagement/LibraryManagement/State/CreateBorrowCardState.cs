74fa512a4a9e37830953d6ebf08c816dfd900e09
Nguyen Phap <phapdeptrai2k2@gmail.com>
2024-06-08 20:21:06 +0700

    done state

---
+++ Done/LibraryManagement/LibraryManagement/State/CreateBorrowCardState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.State
+{
+    public class CreateBorrowCardState : DetailState
+    {
+        public string handle(string id = "", string phieu_id = "", int type = 0)
+        {
+            string msg = "";
+            if (type == 0)
+            {
+                msg = $"Tạo phiếu mượn {phieu_id} thành công!";
+            }
+
+            return msg;
+        }
+    }
+}
---
+++ Done/LibraryManagement/LibraryManagement/State/StateContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.State
+{
+    public class StateContext
+    {
+        private DetailState state;
+
+        public void setState(DetailState state)
+        {
+            this.state = state;
+        }
+
+        public string applyState(string id = "", string phieu_id = "", int type = 0)
+        {
+            return this.state.handle(id, phieu_id, type);
+        }
+    }
+}
